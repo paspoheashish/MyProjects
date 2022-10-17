08aff5a2224cecf84394d78da10d55a7010e232c
paspoheashish <115990218+paspoheashish@users.noreply.github.com>
2022-10-17 16:10:09 +0530

    Add files via upload

---
+++ UserBLL.cs
@@ -0,0 +1,59 @@
+﻿using EntityLayer;
+using InterfaceBusinessLayer;
+using InterfaceDataAccessLayer;
+using System.Collections.Generic;
+
+namespace BusinessLayer
+{
+    public class UserBLL : IUserBLL
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public UserBLL(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+        }
+
+        public List<User> GetUsers()
+        {
+            return unitOfWork.userRepository.Get();
+        }
+
+        public User GetUser(int Id)
+        {
+            return unitOfWork.userRepository.Get(Id);
+        }
+
+        public void SaveUser(User user)
+        {
+            unitOfWork.userRepository.Add(user);
+            unitOfWork.CommitChanges();
+        }
+
+        public void UpdateUser(int id, User user)
+        {
+            unitOfWork.userRepository.Update(id, user);
+            unitOfWork.CommitChanges();
+        }
+
+        public void DeleteUser(int id)
+        {
+            unitOfWork.userRepository.Remove(id);
+            unitOfWork.CommitChanges();
+        }
+
+        public List<User> GetUsers(string role)
+        {
+            return unitOfWork.userRepository.GetUsers(role);
+        }
+
+        public void SaveUsers(List<User> users)
+        {
+            foreach (var user in users)
+            {
+                unitOfWork.userRepository.Add(user);                
+            }
+            unitOfWork.CommitChanges();
+        }
+    }
+}
